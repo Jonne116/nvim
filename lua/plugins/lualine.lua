a7604f1299767a37846d57c5f387845aa4a79093
Jonne Lehtonen <jonne_0@hotmail.com>
2024-06-07 07:59:12 +0300

    Fix config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ require("lualine").setup({
     options = {
         theme = "vscode",
         component_separators = {left = "", right = ""},
-        section_separators = {left = "", right = ""},
+        section_separators = {left = "", right = ""},
     },
     sections = {
         lualine_a = {"mode"},
@@ -14,8 +14,8 @@ require("lualine").setup({
             {
                 function()
                     local msg = "No LSP"
-                    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-                    local clients = vim.lsp.get_active_clients()
+                    local buf_ft = vim.api.nvim_get_option_value("filetype",{})
+                    local clients = vim.lsp.get_clients()
 
                     if next(clients) == nil  then
                         return msg
